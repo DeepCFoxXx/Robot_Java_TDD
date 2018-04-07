4d832e2bf266d7f94e44d4362aae924fa0311da3
DeepCFoxXx <learncode23@gmail.com>
2018-04-07 17:44:05 +0100

    started robot class

--- robot/src/main/java/Robot.java
+++ robot/src/main/java/Robot.java
@@ -1,2 +1,40 @@
+import java.util.HashMap;
+
 public class Robot {
+
+    private int xPosition;
+    private int yPosition;
+    private int prevXposition;
+    private int prevYPosition;
+    private char orientation;
+    private Grid grid;
+    private HashMap<Character, Integer[]> moves;
+    boolean lost;
+
+    public Robot(String initialPosition, Grid grid) {
+        this.setupPosition(initialPosition);
+        this.setUpMoves();
+        this.grid = grid;
+        this.lost = false;
+    }
+
+    private void setupPosition(String pos) {
+        String[] positions = pos.split(" ");
+        this.xPosition = Integer.parseInt(positions[0]);
+        this.yPosition = Integer.parseInt(positions[1]);
+        this.orientation = positions[2].charAt(0);
+    }
+
+    private void setUpMoves() {
+        this.moves = new HashMap<>();
+        Integer[] north = {0, 1};
+        Integer[] east = {1, 0};
+        Integer[] south = {0, -1};
+        Integer[] west = {-1, 0};
+        this.moves.put('N', north);
+        this.moves.put('E', east);
+        this.moves.put('S', south);
+        this.moves.put('W', west);
+    }
+
 }
--- robot/src/test/java/TestRobot.java
+++ robot/src/test/java/TestRobot.java
@@ -1,2 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestRobot {
+
+    Grid grid;
+    Robot robot1;
+    Robot robot2;
+    Robot robot3;
+    Robot robot4;
+
+    @Before
+    public void setUp() {
+        grid = new Grid(5,3);
+        robot1 = new Robot("1 1 E", grid);
+        robot2 = new Robot("1 1 E", grid);
+        robot3 = new Robot("3 2 N", grid);
+        robot4 = new Robot("0 3 W", grid);
+    }
+
+    @Test
+    public void hasStartingPosition() {
+        assertEquals("1 1 E", robot1.getPosition());
+    }
+
 }
